9d97742fc10002058b6c737a2eb68d5cd5471366
main <mhung9911@gmail.com>
2025-09-13 19:56:53 +0700

    test

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,3 @@
 import { Redis } from "@upstash/redis";
 
-// Create a single Redis instance from environment variables.
-// Uses UPSTASH_REDIS_REST_URL and UPSTASH_REDIS_REST_TOKEN.
 export const redis = Redis.fromEnv();
-
