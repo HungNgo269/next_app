470f83838fc22bd305aae9aa3d11d7ea4915ba47
main <mhung9911@gmail.com>
2025-09-13 20:21:31 +0700

    change the way import

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -5,7 +5,6 @@ import {
   upsertSubscriptionPrice,
   upsertSubscriptionProduct,
 } from "@/app/data/subscription";
-import { stripe } from "@/lib/utils/stripe/config";
 import Stripe from "stripe";
 
 const relevantEvents = new Set([
@@ -24,13 +23,14 @@ const relevantEvents = new Set([
 export async function POST(req: Request) {
   const body = await req.text();
   const sig = req.headers.get("stripe-signature") as string;
+  const stripe = await import("@/lib/utils/stripe/config");
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
 
   try {
     if (!sig || !webhookSecret)
       return new Response("Webhook secret not found.", { status: 400 });
-    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+    event = stripe.stripe.webhooks.constructEvent(body, sig, webhookSecret);
     console.log(`🔔  Webhook received: ${event.type}`);
   } catch (err: any) {
     console.log(`❌ Error message: ${err.message}`);
